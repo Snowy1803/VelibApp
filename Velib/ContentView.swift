0921aca5206d0adf3b6a54859050f7ce933665ed
Snowy <3mille.prenom.nom@gmail.com>
2022-06-14 12:48:48 +0200

    Initial Commit

---
+++ Velib/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Velib
+//
+//  Created by Emil Pedersen on 14/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Velib/VelibApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelibApp.swift
+//  Velib
+//
+//  Created by Emil Pedersen on 14/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct VelibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
